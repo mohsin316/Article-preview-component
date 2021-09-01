1b627457b8b6b3fd5f0ab3cb1077fd259f569835
mohsin <mastacode316@gmail.com>
2021-09-01 17:42:33 +0300

    added some transitions

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const imgHolder = document.querySelector('.img-holder');
 const triangle = document.querySelector('.triangle');
 const active = document.querySelector('.active');
+const logoHolder = document.querySelector('.logo-holder')
 
 
 imgHolder.addEventListener('click', () => {
-    active.classList.toggle('hidden');
-    triangle.classList.toggle('hidden');
+    active.classList.toggle('transition');
+    logoHolder.classList.toggle('transition-logos')
+    triangle.classList.toggle('transition');
     imgHolder.classList.toggle('new-color')
 
 });
